cfdeb4b3cd9866ff62a197ab568209db3ca79327
sangatipunithreddy-maker <sangatipunithreddy@gmail.com>
2025-11-12 09:51:08 +0530

    Refactor serializers to improve user handling

--- backend_django/api/serializers.py
+++ backend_django/api/serializers.py
@@ -1,44 +1,47 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import User, Ad, Material, Notification, InterviewPost, ReportedAd
+from .models import Ad, Material, Notification, InterviewPost, ReportedAd
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True, required=True, min_length=4)
     class Meta:
         model = User
-        fields = ('id','name','email','role','year','branch')
-    def get_name(self, obj):
-        return obj.get_full_name() or obj.username
+        fields = ('id','username','email','password','first_name','last_name','role','year','branch')
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
 
 class AdSerializer(serializers.ModelSerializer):
-    seller = UserSerializer(read_only=True)
-    seller_id = serializers.PrimaryKeyRelatedField(source='seller', queryset=User.objects.all(), write_only=True, required=False)
+    owner = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Ad
-        fields = ('id','title','description','price','status','seller','seller_id','created_at','updated_at')
+        fields = ('id','owner','title','description','price','created_at')
 
 class MaterialSerializer(serializers.ModelSerializer):
-    uploaded_by = UserSerializer(read_only=True)
-    uploaded_by_id = serializers.PrimaryKeyRelatedField(source='uploaded_by', queryset=User.objects.all(), write_only=True, required=False)
+    uploader = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Material
-        fields = ('id','subject','semester','academic_year','filename','verified','uploaded_by','uploaded_by_id','created_at','updated_at')
-
-class InterviewPostSerializer(serializers.ModelSerializer):
-    author = UserSerializer(read_only=True)
-    author_id = serializers.PrimaryKeyRelatedField(source='author', queryset=User.objects.all(), write_only=True, required=False)
-    class Meta:
-        model = InterviewPost
-        fields = ('id','title','content','author','author_id','created_at','updated_at')
+        fields = ('id','uploader','name','url','created_at')
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ('id','user','content','read','created_at','updated_at')
+        fields = ('id','message','read','created_at')
+
+class InterviewPostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = InterviewPost
+        fields = ('id','author','title','body','created_at')
 
 class ReportedAdSerializer(serializers.ModelSerializer):
-    ad = AdSerializer(read_only=True)
-    reporter = UserSerializer(read_only=True)
-    ad_id = serializers.PrimaryKeyRelatedField(source='ad', queryset=Ad.objects.all(), write_only=True)
+    reporter = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = ReportedAd
-        fields = ('id','ad','ad_id','reporter','reason','resolved','created_at','updated_at')
+        fields = ('id','ad','reporter','reason','resolved','created_at','updated_at')
